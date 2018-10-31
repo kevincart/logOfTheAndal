b8a3e3834f9e50aca392563c17cd9b3d581e940c
Kevin Cartwright <kevincart@gmail.com>
2018-10-31 22:33:38 +1300

    setup koa, deleted node_modules, oops

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-// Starting place
-require { express };
+const Koa = require('koa');
+const path = require('path');
+const index = require('./view/index.html');
+const app = new Koa();
+
+const main = ctx => {
+    ctx.response.type = 'html';
+    ctx.response.body = fs.createReadStream('./views/index.html');
+};
+
+app.use(main);
+
+if (process.env.NODE_ENV === 'test') {
+  module.exports = app.callback();
+} else {
+  app.listen(3000);
+  console.log('open http://localhost:3000');
+}
+
+app.on('error', function (err) {
+  console.log(err.stack);
+});
